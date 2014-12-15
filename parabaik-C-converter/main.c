f803b52dd83985db9cefad813905f45c94530a4d
Thant <t@ttkz.me>
2014-12-16 03:01:54 +0630

    apply license header and add license files

--- parabaik-C-converter/main.c
+++ parabaik-C-converter/main.c
@@ -1,10 +1,17 @@
+//    This file is part of Parabaik C Converter.
 //
-//  main.c
-//  parabaik-C-converter
+//    Parabaik C Converter is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
 //
-//  Created by Thant Thet Khin Zaw on 12/15/14.
-//  Copyright (c) 2014 myOpenware. All rights reserved.
+//    Foobar is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
 //
+//    You should have received a copy of the GNU General Public License
+//    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <stdio.h>
 #include "parabaik.h"
