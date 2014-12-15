9268ed1b0b935d82bddf916ed95ca5d1bedbe372
Thant <t@ttkz.me>
2014-12-15 21:51:16 +0630

    Initial Commit

---
+++ parabaik-C-converter/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  parabaik-C-converter
+//
+//  Created by Thant Thet Khin Zaw on 12/15/14.
+//  Copyright (c) 2014 myOpenware. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
