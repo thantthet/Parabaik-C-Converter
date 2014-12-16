24caa1c962dd799d8b949c918d6acca29668eedf
Thant <t@ttkz.me>
2014-12-16 23:45:49 +0630

    update license header

--- parabaik-C-converter/NSString+ParabaikConverter.m
+++ parabaik-C-converter/NSString+ParabaikConverter.m
@@ -1,11 +1,11 @@
-//    This file is part of Parabaik C Converter.
+//    Copyright (c) 2014 Parabaik C Converter Project
 //
 //    Parabaik C Converter is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
+//    it under the terms of the GNU Lesser General Public License as published by
 //    the Free Software Foundation, either version 3 of the License, or
 //    (at your option) any later version.
 //
-//    Foobar is distributed in the hope that it will be useful,
+//    Parabaik C Converter is distributed in the hope that it will be useful,
 //    but WITHOUT ANY WARRANTY; without even the implied warranty of
 //    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //    GNU General Public License for more details.
